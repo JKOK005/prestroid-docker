0000d22b8ab2f1f412781acfbadddfd0361833bd
johan.kok <johan.kok@grabtaxi.com>
2020-04-16 18:17:30 +0800

    Unified worker and coordinator configuration to single variable

--- customize-build.sh
+++ customize-build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 build_mode=3 			# 1 - Coordinator only, 2 - Worker only, 3 - Coordinator and worker
-coordinator_config="A"
-worker_config="A"
+config="A"
 jvm_cfg="jvm.config"
 node_cfg="config.properties"
 
@@ -10,9 +9,9 @@ if [ ${build_mode} = 1 ] || [ ${build_mode} = 3 ]
 then
 	echo "Building coordinator image"
 
-	echo "Overwriting config using setting ${coordinator_config}"
-	cp ./customized/configs/coordinator/${coordinator_config}/${jvm_cfg} ./default/etc/${jvm_cfg}
-	cp ./customized/configs/coordinator/${coordinator_config}/${node_cfg} ./default/etc/${node_cfg}
+	echo "Overwriting config using setting ${config}"
+	cp ./customized/configs/coordinator/${config}/${jvm_cfg} ./default/etc/${jvm_cfg}
+	cp ./customized/configs/coordinator/${config}/${node_cfg} ./default/etc/${node_cfg}
 
 	echo "Begin building custom image"
 	docker build -t jkok005/prestroid-coordinator:latest -f ./customized/Dockerfile .
@@ -22,9 +21,9 @@ if [ ${build_mode} = 2 ] || [ ${build_mode} = 3 ]
 then
 	echo "Building worker image"
 
-	echo "Overwriting config using setting ${worker_config}"
-	cp ./customized/configs/worker/${worker_config}/${jvm_cfg} ./default/etc/${jvm_cfg}
-	cp ./customized/configs/worker/${worker_config}/${node_cfg} ./default/etc/${node_cfg}
+	echo "Overwriting config using setting ${config}"
+	cp ./customized/configs/worker/${config}/${jvm_cfg} ./default/etc/${jvm_cfg}
+	cp ./customized/configs/worker/${config}/${node_cfg} ./default/etc/${node_cfg}
 
 	echo "Begin building custom image"
 	docker build -t jkok005/prestroid-worker:latest -f ./customized/Dockerfile .
